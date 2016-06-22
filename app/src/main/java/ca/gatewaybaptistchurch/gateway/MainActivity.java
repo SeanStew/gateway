3e78c8d6b01fa9b265419eb0ea604c74f0d12aa2
Sean Stewart <Sean Stewart>
2016-06-21 22:51:32 -0700

    Basic Firebase implementation. Select and deselect tab color

--- app/src/main/java/ca/gatewaybaptistchurch/gateway/MainActivity.java
+++ app/src/main/java/ca/gatewaybaptistchurch/gateway/MainActivity.java
@@ -9,10 +9,14 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import net.steamcrafted.materialiconlib.MaterialDrawableBuilder;
+import net.steamcrafted.materialiconlib.MaterialIconView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,103 +24,141 @@ import ca.gatewaybaptistchurch.gateway.adapter.HomeAdapter;
 import ca.gatewaybaptistchurch.gateway.view.NonSwipeableViewPager;
 
 public class MainActivity extends AppCompatActivity {
-	@BindView(R.id.drawer_layout) DrawerLayout drawer;
-	@BindView(R.id.nav_view) NavigationView navigationView;
-	@BindView(R.id.toolbar) Toolbar toolbar;
-	@BindView(R.id.tab_layout) TabLayout tabLayout;
-	@BindView(R.id.view_pager) NonSwipeableViewPager viewPager;
-	@BindView(R.id.fab) FloatingActionButton fab;
+    @BindView(R.id.drawer_layout) DrawerLayout drawer;
+    @BindView(R.id.nav_view) NavigationView navigationView;
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.tab_layout) TabLayout tabLayout;
+    @BindView(R.id.view_pager) NonSwipeableViewPager viewPager;
+    @BindView(R.id.fab) FloatingActionButton fab;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		ButterKnife.setDebug(true);
-		ButterKnife.bind(this);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.setDebug(true);
+        ButterKnife.bind(this);
 
-		setupViews();
-		setupViewPager();
-	}
+        setupViews();
+        setupViewPager();
+    }
 
-	private void setupViews() {
-		setSupportActionBar(toolbar);
-		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-		drawer.addDrawerListener(toggle);
-		toggle.syncState();
+    private void setupViews() {
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
-		navigationView.setNavigationItemSelectedListener(navigationItemSelectedListener);
-	}
+        navigationView.setNavigationItemSelectedListener(navigationItemSelectedListener);
+    }
 
-	private void setupViewPager() {
-		HomeAdapter adapter = new HomeAdapter(getSupportFragmentManager());
-		adapter.addFragment(new MessagesFragment(), "Messages");
-		adapter.addFragment(new MessagesFragment(), "Bible");
-		adapter.addFragment(new MessagesFragment(), "News");
-		adapter.addFragment(new MessagesFragment(), "Connect");
-		adapter.addFragment(new MessagesFragment(), "Give");
-		viewPager.setAdapter(adapter);
-		viewPager.setOffscreenPageLimit(4);
-		tabLayout.setupWithViewPager(viewPager);
+    private void setupViewPager() {
+        HomeAdapter adapter = new HomeAdapter(getSupportFragmentManager());
+        adapter.addFragment(new MessagesFragment(), "Messages");
+        adapter.addFragment(new MessagesFragment(), "Bible");
+        adapter.addFragment(new MessagesFragment(), "News");
+        adapter.addFragment(new MessagesFragment(), "Connect");
+        adapter.addFragment(new MessagesFragment(), "Give");
+        viewPager.setAdapter(adapter);
+        viewPager.setOffscreenPageLimit(4);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setOnTabSelectedListener(onTabSelected);
 
-		tabLayout.getTabAt(0).setIcon(MaterialDrawableBuilder.with(this).setIcon(MaterialDrawableBuilder.IconValue.NEWSPAPER).setColorResource(R.color.icons).setToActionbarSize().build());
-		tabLayout.getTabAt(1).setIcon(MaterialDrawableBuilder.with(this).setIcon(MaterialDrawableBuilder.IconValue.BOOK_OPEN_VARIANT).setColorResource(R.color.icons).setToActionbarSize().build());
-		tabLayout.getTabAt(2).setIcon(MaterialDrawableBuilder.with(this).setIcon(MaterialDrawableBuilder.IconValue.CALENDAR_TEXT).setColorResource(R.color.icons).setToActionbarSize().build());
-		tabLayout.getTabAt(3).setIcon(MaterialDrawableBuilder.with(this).setIcon(MaterialDrawableBuilder.IconValue.ACCOUNT_MULTIPLE).setColorResource(R.color.icons).setToActionbarSize().build());
-		tabLayout.getTabAt(4).setIcon(MaterialDrawableBuilder.with(this).setIcon(MaterialDrawableBuilder.IconValue.CREDIT_CARD).setColorResource(R.color.icons).setToActionbarSize().build());
-	}
+        setupIcon(0, MaterialDrawableBuilder.IconValue.NEWSPAPER, adapter.getPageTitle(0).toString());
+        setupIcon(1, MaterialDrawableBuilder.IconValue.BOOK_OPEN_VARIANT, adapter.getPageTitle(1).toString());
+        setupIcon(2, MaterialDrawableBuilder.IconValue.CALENDAR_TEXT, adapter.getPageTitle(2).toString());
+        setupIcon(3, MaterialDrawableBuilder.IconValue.ACCOUNT_MULTIPLE, adapter.getPageTitle(3).toString());
+        setupIcon(4, MaterialDrawableBuilder.IconValue.CREDIT_CARD, adapter.getPageTitle(4).toString());
+        setTabSelected(tabLayout.getTabAt(0).getCustomView(), true);
+    }
 
-	@Override
-	public void onBackPressed() {
-		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-		if (drawer.isDrawerOpen(GravityCompat.START)) {
-			drawer.closeDrawer(GravityCompat.START);
-		} else {
-			super.onBackPressed();
-		}
-	}
+    private void setupIcon(int position, MaterialDrawableBuilder.IconValue iconValue, String title) {
+        View rootView = LayoutInflater.from(this).inflate(R.layout.tab_custom, null);
+        TextView textView = (TextView) rootView.findViewById(R.id.tab_text);
+        MaterialIconView iconView = (MaterialIconView) rootView.findViewById(R.id.tab_icon);
+        textView.setText(title);
+        iconView.setIcon(iconValue);
+        tabLayout.getTabAt(position).setCustomView(rootView);
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+    private void setTabSelected(View rootView, boolean selected){
+        int color = selected ? getResources().getColor(R.color.icons) : getResources().getColor(R.color.iconsDisabled);
+        TextView textView = (TextView) rootView.findViewById(R.id.tab_text);
+        MaterialIconView iconView = (MaterialIconView) rootView.findViewById(R.id.tab_icon);
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
+        textView.setTextColor(color);
+        iconView.setColor(color);
+    }
 
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-	//Listeners
-	NavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new NavigationView.OnNavigationItemSelectedListener() {
-		@Override
-		public boolean onNavigationItemSelected(MenuItem item) {
-			// Handle navigation view item clicks here.
-			int id = item.getItemId();
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-			if (id == R.id.nav_about) {
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
 
-			} else if (id == R.id.nav_help) {
+        return super.onOptionsItemSelected(item);
+    }
 
-			} else if (id == R.id.nav_settings) {
+    //<editor-fold desc="Listeners">
+    TabLayout.OnTabSelectedListener onTabSelected = new TabLayout.OnTabSelectedListener() {
+        @Override
+        public void onTabSelected(TabLayout.Tab tab) {
+           setTabSelected(tab.getCustomView(), true);
+        }
 
-			} else if (id == R.id.nav_share) {
+        @Override
+        public void onTabUnselected(TabLayout.Tab tab) {
+            setTabSelected(tab.getCustomView(), false);
+        }
 
-			}
+        @Override
+        public void onTabReselected(TabLayout.Tab tab) {
 
-			DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-			drawer.closeDrawer(GravityCompat.START);
-			return true;
-		}
-	};
+        }
+    };
+
+    NavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new NavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+            // Handle navigation view item clicks here.
+            int id = item.getItemId();
+
+            if (id == R.id.nav_about) {
+
+            } else if (id == R.id.nav_help) {
+
+            } else if (id == R.id.nav_settings) {
+
+            } else if (id == R.id.nav_share) {
+
+            }
+
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
+        }
+    };
+    //</editor-fold>
 }
---
+++ app/src/main/java/ca/gatewaybaptistchurch/gateway/service/GatewayFirebaseMessagingService.java
@@ -0,0 +1,51 @@
+package ca.gatewaybaptistchurch.gateway.service;
+
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import ca.gatewaybaptistchurch.gateway.MainActivity;
+import ca.gatewaybaptistchurch.gateway.R;
+import timber.log.Timber;
+
+public class GatewayFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // TODO(developer): Handle FCM messages here.
+        // If the application is in the foreground handle both data and notification messages here.
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+        Timber.d("From: %s", remoteMessage.getFrom());
+        Timber.d("Notification Message Body: %s", remoteMessage.getNotification().getBody());
+        sendNotification(remoteMessage.getNotification());
+    }
+
+    private void sendNotification(RemoteMessage.Notification notification) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_help_black_24dp)
+                .setContentTitle(notification.getTitle())
+                .setContentText(notification.getBody())
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+}
